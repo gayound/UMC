33c1e351b301329652712244281d54c59619cd8a
gayoung <70945806+gayound@users.noreply.github.com>
2024-04-08 12:05:32 +0900

    1.platform

---
+++ 1. platform/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  1. platform
+//
+//  Created by 김가영 on 4/8/24.
+//
+
+import UIKit
+
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var myView: UIView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+}
