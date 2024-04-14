866f1ab9d6db3ad4be1f624dbdaddcdca5d68790
gayoung <70945806+gayound@users.noreply.github.com>
2024-04-14 23:54:07 +0900

    Add files via upload

---
+++ 1.baemin/1.baemin/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  1.baemin
+//
+//  Created by 김가영 on 4/9/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
